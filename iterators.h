92a8c4dfccde61eea249847b6bcecef1ce5611e5
AliZahidi <a.alizahidi@gmail.com>
2019-09-13 15:22:33 +0200

    Nooby organisation

---
+++ iterators.h
@@ -0,0 +1,87 @@
+
+class Iterator_on_vertices : public std::iterator<std::input_iterator_tag, Face>
+{
+    int index;
+    Mesh *m;
+
+public:
+    Iterator_on_vertices(int x, Mesh *mesh) : index(x), m(mesh) {}
+    Iterator_on_vertices &operator++()
+    {
+        index++;
+        return *this;
+    }
+    Vertex *operator*() { return m->getVertex(index); }
+};
+
+class Circulator_on_vertices : public std::iterator<std::input_iterator_tag, Face>
+{
+    int currentFaceIndex;
+    int axisVertexIndex;
+    int currentAdjacentVertex;
+    Mesh *m;
+
+public:
+    Circulator_on_vertices(int faceIndex, int axisIndex, Mesh *mesh) : currentFaceIndex(faceIndex),
+                                                                       axisVertexIndex(axisIndex), m(mesh)
+    {
+        int axisLocalIndex = m->getFace(currentFaceIndex)->global2localIndex(axisVertexIndex);
+        currentAdjacentVertex = m->getFace(currentFaceIndex)->getVertex((axisLocalIndex + 1) % 3);
+    }
+    Circulator_on_vertices(int axisIndex, Mesh *mesh) : currentFaceIndex(mesh->getVertex(axisIndex)->getFaceIndex()),
+                                                                       axisVertexIndex(axisIndex), m(mesh)
+    {
+        int axisLocalIndex = m->getFace(currentFaceIndex)->global2localIndex(axisVertexIndex);
+        currentAdjacentVertex = m->getFace(currentFaceIndex)->getVertex((axisLocalIndex + 1) % 3);
+    }
+
+    Circulator_on_vertices &operator++()
+    {
+        int axisLocalIndex = m->getFace(currentFaceIndex)->global2localIndex(axisVertexIndex);
+        currentFaceIndex = m->getFace(currentFaceIndex)->getNeibFace((axisLocalIndex + 1) % 3);
+        return *this;
+    }
+
+    Vertex *operator*() { return m->getVertex(currentFaceIndex); }
+};
+
+class Iterator_on_faces : public std::iterator<std::input_iterator_tag, Face>
+{
+    int index;
+    Mesh *m;
+
+public:
+    Iterator_on_faces(int x, Mesh *mesh) : index(x), m(mesh) {}
+    //Todo Make the face iteration trigonometric
+    Iterator_on_faces &operator++()
+    {
+        index++;
+        return *this;
+    }
+
+    Face *operator*() { return m->getFace(index); }
+};
+
+class Circulator_on_faces : public std::iterator<std::input_iterator_tag, Face>
+{
+    int currentFaceIndex;
+    int axisVertexIndex;
+    Mesh *m;
+
+public:
+    Circulator_on_faces(int faceIndex, int axisIndex, Mesh *mesh) : currentFaceIndex(faceIndex),
+                                                                    axisVertexIndex(axisIndex), m(mesh) {}
+
+    Circulator_on_faces(int axisIndex, Mesh *mesh) : axisVertexIndex(axisIndex), m(mesh)
+    {
+        currentFaceIndex = m->getVertex(axisIndex)->getFaceIndex();
+    }
+
+    Circulator_on_faces &operator++()
+    {
+        int axisLocalIndex = m->getFace(currentFaceIndex)->global2localIndex(axisVertexIndex);
+        currentFaceIndex = m->getFace(currentFaceIndex)->getNeibFace((axisLocalIndex + 1) % 3);
+        return *this;
+    }
+    Face *operator*() { return m->getFace(currentFaceIndex); }
+};
---
+++ segmentMemory.h
@@ -0,0 +1,60 @@
+class SegmentMemory{
+    class SegmentMemoryKey{
+    public:
+        int vertexIndex1;
+        int vertexIndex2;
+        SegmentMemoryKey(int index1, int index2)
+            : vertexIndex1(index1), vertexIndex2(index2){
+            if(index1 < index2){
+                vertexIndex1 = index1;
+                vertexIndex2 = index2;
+            } else {
+                vertexIndex1 = index2;
+                vertexIndex2 = index1;
+            }
+        }
+
+        bool operator==(const SegmentMemoryKey& cmp) const{
+            return (this->vertexIndex1==cmp.vertexIndex2&&this->vertexIndex2==cmp.vertexIndex1);
+        }
+
+        bool operator<(const SegmentMemoryKey& cmp) const{
+            return ((this->vertexIndex1<cmp.vertexIndex1)
+                    ||(this->vertexIndex1==cmp.vertexIndex1&&this->vertexIndex2<cmp.vertexIndex2));
+        }
+
+    };
+
+    class SegmentMemoryData{
+    public:
+        int faceIndex;
+        int vertexInFaceIndex;
+        SegmentMemoryData(int fIndex, int vertexIndex)
+            : faceIndex(fIndex), vertexInFaceIndex(vertexIndex){}
+    };
+public:
+    QMap<SegmentMemoryKey, SegmentMemoryData> hashMap;
+
+    void addSegment(int vI1, int vI2, int fI, int vIFI){
+        hashMap.insert(SegmentMemoryKey(vI1, vI2), SegmentMemoryData(fI, vIFI));
+    }
+
+    void deleteSegment(const int cmp[2]){
+        SegmentMemoryKey toDelete(cmp[0], cmp[1]);
+        QMap<SegmentMemoryKey, SegmentMemoryData>::iterator it = hashMap.find(toDelete);
+        hashMap.erase(it);
+    }
+
+    bool contain(const int cmp[2]){
+        return hashMap.contains(SegmentMemoryKey(cmp[0], cmp[1]));
+    }
+
+    int faceIndex(const int cmp[2]){
+        return (hashMap.find(SegmentMemoryKey(cmp[0], cmp[1]))).value().faceIndex;
+    }
+
+    int vertexInFaceIndex(const int cmp[2]){
+        return (hashMap.find(SegmentMemoryKey(cmp[0], cmp[1]))).value().vertexInFaceIndex;
+    }
+};
+
